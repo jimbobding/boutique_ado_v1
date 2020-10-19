db736f54864a9f79427158f87745e43513b59ec6
jimbobding <jimmygrahamdavid@gmail.com>
2020-10-19 12:07:42 +0000

    changed settings.py file fixing a bug

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -129,15 +129,15 @@ WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
-       ' default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
 else:
- DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': BASE_DIR / 'db.sqlite3',
-   }
-  }
+  DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
