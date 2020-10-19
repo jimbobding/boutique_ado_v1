c2f2ff9476dbdf1e1e08bc0f42c413e7a9df5999
jimbobding <jimmygrahamdavid@gmail.com>
2020-10-19 14:31:10 +0000

    changed aws storage configs to settings.py

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -198,8 +198,8 @@ if USE_AWS in os.environ:
     MEDIAFILES_LOACTION = 'media'
 
     # Override static and media URLs in production
-    STATIC_URL = F'https://{AWS_S3_CUSTOM_DOMAIN}//{STATICFILES_LOCATION}'
-    MEDIA_URL = F'https://{AWS_S3_CUSTOM_DOMAIN}//{MEDIAFILES_LOCATION}'
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
 
 # Stripe
